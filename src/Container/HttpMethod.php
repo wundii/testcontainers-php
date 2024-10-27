662acfefa102edb63cbe9e2cf3b754b63edc7584
Sergei Shitikov <rw4lll@gmail.com>
2024-10-27 21:32:47 +0100

    Update src/Container/HttpMethod.php
    improve fromString
    
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

--- src/Container/HttpMethod.php
+++ src/Container/HttpMethod.php
@@ -15,14 +15,6 @@ enum HttpMethod: string
 
     public static function fromString(string $method): self
     {
-        return match (strtoupper($method)) {
-            'GET' => self::GET,
-            'POST' => self::POST,
-            'PUT' => self::PUT,
-            'DELETE' => self::DELETE,
-            'HEAD' => self::HEAD,
-            'OPTIONS' => self::OPTIONS,
-            default => throw new \InvalidArgumentException("Invalid HTTP method: $method"),
-        };
+        return self::tryFrom(strtoupper($method)) ?? throw new \InvalidArgumentException("Invalid HTTP method: $method");
     }
 }
