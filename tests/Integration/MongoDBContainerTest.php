4134cf951d2cef247e65adbef2be3eb23dc051da
stan220 <stas@aliferovich.com>
2025-04-24 17:17:29 +0300

    Add mongodb module

---
+++ tests/Integration/MongoDBContainerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Integration;
+
+use Testcontainers\Container\StartedGenericContainer;
+use Testcontainers\Modules\MongoDBContainer;
+use Testcontainers\Tests\Integration\ContainerTestCase;
+
+class MongoDBContainerTest extends ContainerTestCase
+{
+    public function setUp(): void
+    {
+        $this->container = (new MongoDBContainer())->start();
+    }
+
+    public function testMongoDBContainer(): void
+    {
+        self::assertInstanceOf(StartedGenericContainer::class, $this->container);
+
+        $pingResult = $this->container->exec([
+            'mongosh', 'admin',
+            '-u', 'test',
+            '-p', 'test',
+            '--eval', "'db.runCommand(\"ping\").ok'",
+        ]);
+
+        self::assertEquals(1, $pingResult);
+    }
+}
